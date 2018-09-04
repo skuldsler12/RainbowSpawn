6b02b80aa610a9ae9f6274df318adb9e6003ebe0
DarkByx <atanacio398@gmail.com>
2018-09-03 23:04:53 -0500

    Main
    change data for rank vip/vip+ etc

--- src/Skuld/Main.php
+++ src/Skuld/Main.php
@@ -1,64 +1,55 @@
 <?php
 
 namespace Skuld;
+
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-class Main extends PluginBase implements Listener{
-       
-        public function onEnable(){
-                $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getLogger()->info("¡RainbowSpawnProtect Cargado!");
-		$this->Data = new Config($this->getDataFolder() . "vips.json", Config::JSON);
-               
+use pocketmine\utils\TextFormat as TE;
+
+class Main extends PluginBase implements Listener {
+
+    public $usrActv = 0;
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info("¡RainbowSpawnProtect Cargado!");
+        $this->Data = new Config($this->getDataFolder() . "vips.json", Config::JSON);
+    }
+
+    public function onPlayerLogin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $playername = $player->getName();
+        $entra = false;
+        $maxUSR = 0;
+        $online = count($this->getServer()->getOnlinePlayers());
+        if($this->checkData($playername)) {
+            $rank = $this->getRank($playername);
+            $player->setNameTag(TE::GRAY."[".TE::GOLD.$rank.TE::GRAY."]".TE::AQUA.$playername);
+            $player->addAttachment($this, "test.command");
+        }else{
+            if ($online < $maxUSR) {
+                $event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+                self::$usrActv++;
+            }else{
+                $event->getPlayer()->kick("Servidor completo... Prueba en 10 minutos.",true,"Servidor completo... Prueba en 10 minutos.");
+                $event->setCancelled();
+            }
         }
-        public function onPlayerLogin(PlayerJoinEvent $event){
-                $playername = $event->getPlayer()->getName();
-                $entra = false;
-                $maxUSR = 0;
-                $usrActv = count($this->getServer()->getOnlinePlayers());
-                if($this->checkData($playername)){
-			$rank = $this->getRank($playername);
-		}else{
-			//create rank data
-		}
-                    
-		/*$this->getServer()->getLogger()->info("¡Player entrando!");
-                foreach ($viplist as $k => $v) {
-                       if ($k === $playername) {
-                               if ($v) {
-                               $entra = $v; 
-                               }
-                       }
-                }*/#unnecesary
-                if ($entra) {
-                        $event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-                        $usrActv += 1;
-                } else {
-                        if ($usrActv <$maxUSR) {
-                                $event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-                                $usrActv += 1;
-                        }else{
-                                //$event->getPlayer()->kick("Servidor completo... Prueba en 10 minutos.",true,"Servidor completo... Prueba en 10 minutos.");
-                                $event->setCancelled();
-                        }
-                }
-                
-		
-        }
-	
-	public function checkData($username) {
-		return $this->Data->exists($username);
-	}
-        
-	public function getRank($username) {
-		return $this->Data->get($username);
-	}
-	
-        public function onPlayerQuit(PlayerQuitEvent $event){
-                $usrActv -= 1;
-        }
-       
+    }
+
+    public function checkData($username) {
+        return $this->Data->exists($username);
+    }
+
+    public function getRank($username) {
+        return $this->Data->get($username);
+    }
+
+    public function onPlayerQuit(PlayerQuitEvent $event){
+        self::$usrActv--;
+    }
+
 }
